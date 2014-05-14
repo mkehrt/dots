e24781a4c6b2be05b35431e4bc0801b86088f5c7
Matthew Kehrt <mkehrt@roseandsigil.net>
2014-05-13 17:31:37 -0700

    Move some stuff; ssh completion

--- .bash/completion.bash
+++ .bash/completion.bash
@@ -1,1 +1,6 @@
 #! /bin/bash
+
+alias comp="complete -o bashdefault"
+
+# ssh from know_hosts.  Idea from Blackmad.
+comp -W "$(cat ~/.ssh/known_hosts | sed -e "s/\([^,]*\).*/\\1/" | grep -v ==)" ssh scp rsync
---
+++ .bash/path.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export PATH=${HOME}/bin:/opt/local/bin:/opt/local/sbin:${HOME}/Library/Haskell/bin:${PATH}
